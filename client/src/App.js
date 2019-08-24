cb2be25897fa45d0dc14274e8510ad1c055c14a9
anagutjor <anagutjor@gmail.com>
2019-08-25 01:53:34 +0200

    add aragon modules & migrate

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
-
+import { Button } from '@aragon/ui'
 import "./App.css";
 
 class App extends Component {
@@ -53,18 +53,9 @@ class App extends Component {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div>
+        <Button mode="outline">Cancel</Button>
+        <Button mode="strong">Accept</Button>
       </div>
     );
   }
